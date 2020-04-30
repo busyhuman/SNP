7aab347bb66bf3dbc60b59cd53747dc1f84d8460
busyhuman <busyhuman@naver.com>
2020-04-30 16:57:01 +0900

    Update settings.py

--- web_proj/settings.py
+++ web_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rqzy==#b$m2#*b%a)shw##9&99r!qqj59_q%_9p+*v7g^c2#=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
